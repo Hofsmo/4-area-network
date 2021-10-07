44896cc861ea58b1cf0c1238bf9261be5a90e541
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2021-10-07 14:03:22 +0200

    Added loads to the pandapower case

--- convert_to_mat.m
+++ convert_to_mat.m
@@ -1,16 +1,26 @@
 %% Create mpc struct for the 4-area network
-mpc = struct('bus',[], 'branch',[], 'gen',[], 'gencost',[], 'areas',[]);
+mpc = struct('bus',[], 'branch',[], 'gen',[], 'gencost',[], 'areas',[],...
+    'loaddata', []);
 files = fieldnames(mpc);
 for i = 1:numel(files)
     file = files{i};
     if file == "gencost"
-        skip = 2;
+        R = 2;
+        C = 0;
+    elseif file == "loaddata"
+        R = 2;
+        C = 1;
     else
-        skip = 1;
+        R = 1;
+        C= 0;
     end
-    mpc.(file) = dlmread(sprintf("4area_network_%s.csv", file), ';', skip, 0);
+    mpc.(file) = dlmread(sprintf("4area_network_%s.csv", file), ';', R, C);
 end
 mpc.version=2;
 mpc.baseMVA = 100;
+
+% Set the load to the first operating state in the loaddata
+load_idx = ismember(mpc.bus(:,1), mpc.loaddata(:, 1));
+mpc.bus(load_idx, 3) = mpc.loaddata(:, 4);
 
 save("4_area", "mpc")
